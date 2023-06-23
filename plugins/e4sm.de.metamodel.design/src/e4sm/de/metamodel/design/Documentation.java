5fa7d18a5a4e1049439dfc9cf7bdf4a0182c84b5
frbe5612 <francesco.bedini@tu-ilmenau.de>
2023-06-23 15:54:15 +0200

    Add platform independent properties

--- plugins/e4sm.de.metamodel.design/src/e4sm/de/metamodel/design/Documentation.java
+++ plugins/e4sm.de.metamodel.design/src/e4sm/de/metamodel/design/Documentation.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.util.Collections;
 
 import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
-import e4sm.de.metamodel.e4sm.Model;
-
 import e4sm.de.metamodel.acceleo.documentation.main.Generate;
+import e4sm.de.metamodel.e4sm.Model;
 
 // Currently unused (did not work)
 // right click on the model file, and select:
@@ -18,7 +18,7 @@ public class Documentation {
 	private Generate g;
 
 	public Boolean generateDocumentation(Model m) {
-		
+
 		Utils.debug("\n\n====================\nGenerate documentation\n====================\n");
 		var inputURI = EcoreUtil.getURI(m);
 		Utils.debug(inputURI.toString());
@@ -38,15 +38,14 @@ public class Documentation {
 		Utils.debug("\nProject path: " + projectPath);
 		Utils.debug("\nModel file name: " + modelFileName);
 
-			
-	    // Using the default output SCPN/model_name.xml"
-		//var outputPath = projectPath + "E4SM_Scenario1/doc/";
-		var outputPath =  "E4SM_Scenario1/doc/";
-		
+		// Using the default output SCPN/model_name.xml"
+		// var outputPath = projectPath + "E4SM_Scenario1/doc/";
+		var outputPath = "E4SM_Scenario1/doc/";
+
 		File targetDir = new File(outputPath);
 		try {
 			Utils.debug("\nGenerating the documentation at the path: " + targetDir);
-			g = new Generate(m, targetDir, Collections.emptyList());
+			g = new Generate((EObject) m, targetDir, Collections.emptyList());
 			Utils.debug("\nobject created");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -54,7 +53,9 @@ public class Documentation {
 			e.printStackTrace();
 			return false;
 		}
+
 		var monitor = new BasicMonitor();
+
 		try {
 			Utils.debug("\nStart generation");
 			g.doGenerate(monitor);
@@ -64,6 +65,7 @@ public class Documentation {
 			e.printStackTrace();
 			return false;
 		}
+
 		return true;
 	}
 }
